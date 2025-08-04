bb8b0875f9442aff20c7047a647f0d022229b74e
lakshay15699 <blakshay66@gmail.com>
2025-08-04 07:09:31 +0530

    Update index.js with proper MongoDB connection

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/api/posts', postRoutes);
 
 // DB Connection
 mongoose
-.connect(process.env.MONGODB_URI, {
+.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
